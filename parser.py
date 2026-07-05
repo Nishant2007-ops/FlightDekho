17d4d13c78922546a8b85d8bce0062d576e06b7a
Nishant Ladwal <nishantladwal@Nishants-MacBook-Air.local>
2026-07-05 19:36:30 +0530

    Added Airline name mapping in parser

--- parser.py
+++ parser.py
@@ -1,5 +1,57 @@
 import pandas as pd
 
+AIRLINES = {
+    "AI": "Air India",
+    "6E": "IndiGo",
+    "SG": "SpiceJet",
+    "UK": "Akasa Air",
+    "IX": "Air India Express",
+    "QP": "Akasa Air Express",
+    "I5": "AirAsia India",
+    "G8": "Go First",
+    "EK": "Emirates",
+    "EY": "Etihad Airways",
+    "QR": "Qatar Airways",
+    "SV": "Saudia",
+    "KU": "Kuwait Airways",
+    "GF": "Gulf Air",
+    "WY": "Oman Air",
+    "RJ": "Royal Jordanian",
+    "BA": "British Airways",
+    "LH": "Lufthansa",
+    "AF": "Air France",
+    "KL": "KLM",
+    "LX": "Swiss International Air Lines",
+    "OS": "Austrian Airlines",
+    "IB": "Iberia",
+    "AZ": "ITA Airways",
+    "AY": "Finnair",
+    "SK": "Scandinavian Airlines",
+    "TP": "TAP Air Portugal",
+    "EI": "Aer Lingus",
+    "AA": "American Airlines",
+    "DL": "Delta Air Lines",
+    "UA": "United Airlines",
+    "WN": "Southwest Airlines",
+    "AS": "Alaska Airlines",
+    "B6": "JetBlue Airways",
+    "AC": "Air Canada",
+    "WS": "WestJet",
+    "SQ": "Singapore Airlines",
+    "CX": "Cathay Pacific",
+    "TG": "Thai Airways",
+    "MH": "Malaysia Airlines",
+    "JL": "Japan Airlines",
+    "NH": "All Nippon Airways",
+    "KE": "Korean Air",
+    "OZ": "Asiana Airlines",
+    "CI": "China Airlines",
+    "BR": "EVA Air",
+    "CZ": "China Southern Airlines",
+    "MU": "China Eastern Airlines",
+    "CA": "Air China"
+}
+
 def flight_dataframe(data):
 
     rows = []
@@ -10,9 +62,12 @@ def flight_dataframe(data):
         if flight.get("cs_flight_iata") is not None:
             continue
 
+        airline_code = flight.get("airline_iata")
+        airline_name = AIRLINES.get(airline_code, airline_code)
+
         rows.append({
 
-            "Airline": flight.get("airline_iata"),
+            "Airline": airline_name,
 
             "Flight": flight.get("flight_iata"),
 
